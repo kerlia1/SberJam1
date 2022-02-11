8ad3128f979c9df952a20babdf18a47efeb233a6
slivoviy <s01110011@yandex.ru>
2022-02-11 18:37:27 +0300

    Buffer.cs

--- Assets/Scripts/Player Scripts/Buffer.cs
+++ Assets/Scripts/Player Scripts/Buffer.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Player_Scripts {
     public class Buffer : MonoBehaviour {
+        [SerializeField] private List<GameObject> buttons;
+        [SerializeField] private GameObject gameController;
 
         private Player player;
 
@@ -14,58 +18,93 @@ namespace Player_Scripts {
         //1. Купить готовые решения (DamagePerClick * 3, Timer = 10s, Cost = 100$)
         //3. Бахнуть энергос (DamagePerClick * 2, Timer = 10s, Cost = 25$)
         public void BuySolution() {
-            player.Money -= 100;
-            Debug.Log($"У игрока теперь денег: {player.Money}");
-            StartCoroutine(BuffDPC(player.DamagePerClick,
-                player.DamagePerClick * 3, 10f));
+            if (player.Money >= 100) {
+                player.Money -= 100;
+                Debug.Log($"У игрока теперь денег: {player.Money}");
+                StartCoroutine(BuffDPC(player.DamagePerClick,
+                    player.DamagePerClick * 3, 10f, 0));
+            }
+            else {
+                Debug.Log($"У тебя нет денег, лох");
+            }
         }
         
         public void DrinkEnergy() {
-            player.Money -= 25;
-            Debug.Log($"У игрока теперь денег: {player.Money}");
-            StartCoroutine(BuffDPC(player.DamagePerClick,
-                player.DamagePerClick * 2, 10f));
+            if (player.Money >= 25) {
+                player.Money -= 25;
+                Debug.Log($"У игрока теперь денег: {player.Money}");
+                StartCoroutine(BuffDPC(player.DamagePerClick,
+                    player.DamagePerClick * 2, 10f, 1));
+            }
+            else {
+                Debug.Log($"У тебя нет денег, лох");
+            }
         }
 
-        public IEnumerator BuffDPC(float previous, float temporary, float seconds) {
+        public IEnumerator BuffDPC(float previous, float temporary, float seconds, int buttonIndex) {
             player.DamagePerClick = temporary;
+            buttons[buttonIndex].GetComponent<Button>().interactable = false;
             Debug.Log($"Урон по клику: {player.DamagePerClick}");
             
             yield return new WaitForSeconds(seconds);
 
             player.DamagePerClick = previous;
+            buttons[buttonIndex].GetComponent<Button>().interactable = true;
             Debug.Log($"Урон по клику {player.DamagePerClick}");
         }
 
         //2. Выпить пива (MentalHealth + 5, Cost = 30$)
         public void DrinkBeer() {
-            var mentalHealth = player.MentalHealth + 5;
-            if (mentalHealth > player.MaxMentalHealth) {
-                player.MentalHealth = player.MaxMentalHealth;
+            if (player.Money >= 30) {
+                player.Money -= 30;
+                var mentalHealth = player.MentalHealth + 5;
+                if (mentalHealth > player.MaxMentalHealth) {
+                    player.MentalHealth = player.MaxMentalHealth;
+                }
+                else {
+                    player.MentalHealth = mentalHealth;
+                }
+
+                Debug.Log($"Менталка: {player.MentalHealth}");
+                Debug.Log($"У игрока теперь денег: {player.Money}");
             }
             else {
-                player.MentalHealth = mentalHealth;
+                Debug.Log($"У тебя нет денег, лох");
             }
-            Debug.Log($"Менталка: {player.MentalHealth}");
-            
-            player.Money -= 30;
-            Debug.Log($"У игрока теперь денег: {player.Money}");
         }
 
-        //4. ВОйти в HARDMODE (DamagePerClick * 3, Timer = 10s, Cost = MentalHealth - 7)
+        //4. Войти в HARDMODE (DamagePerClick * 3, Timer = 10s, Cost = MentalHealth - 7)
         public void HardMode() {
             player.MentalHealth -= 7;
-            StartCoroutine(BuffDPC(player.DamagePerClick, player.DamagePerClick * 3, 10f));
+            StartCoroutine(BuffDPC(player.DamagePerClick,
+                player.DamagePerClick * 3, 10f, 3));
         }
         
         //5. Нанять индуса (DamagePerSecond + 0.5, Timer = 120s, Cost = 200$)
-        public void BuffDPS(int sum, float seconds, int cost) {
-            
+        public void HireIndus() {
+            if (player.Money >= 200) {
+                player.Money -= 200;
+                StartCoroutine(BuffDPS(player.DamagePerSecond,
+                    player.DamagePerSecond + 0.5f, 120));
+            }
         }
 
-        //6. Закрыть задачу (Закончить задачу??, Cost = 300$)
-        public void BuyWin(int cost) {
+        private IEnumerator BuffDPS(float previous, float temporary, float seconds) {
+            player.DamagePerSecond = temporary;
+            buttons[4].GetComponent<Button>().interactable = false;
+            Debug.Log($"Пассивный дамаг: {player.DamagePerSecond}");
             
+            yield return new WaitForSeconds(seconds);
+
+            player.DamagePerSecond = previous;
+            buttons[4].GetComponent<Button>().interactable = true;
+            Debug.Log($"Пассивный дамаг: {player.DamagePerSecond}");
+        }
+        
+
+        //6. Закрыть задачу (Закончить задачу??, Cost = 300$)
+        public void BuyWin() {
+            gameController.GetComponent<GameController>().ExitToHub("win");
         }
     }
 }
